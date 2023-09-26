ee2e04a2e06712dcf2982c008d0deef890bb5804
benbobyabraham <benbobyabraham@gmail.com>
2023-09-26 17:26:09 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from rest_framework import generics
+from requests.models import Response
+
+class testapi(APIView):
+
+    def get(self,request):
+        
+        data = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+        
+        return JsonResponse(data)
